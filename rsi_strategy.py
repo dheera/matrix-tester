ac1b1c1730247f60ee7b2d67c7959c0aae8da7be
dheera <dheera@dheera.net>
2025-02-24 19:41:38 +0000

    rm crap

--- rsi_strategy.py
+++ rsi_strategy.py
@@ -14,12 +14,6 @@ class RSIStrategy(Strategy):
         self.overbought = overbought
         self.oversold = oversold
 
-    def on_start_day(self):
-        print("Day has started!")
-
-    def on_end_day(self):
-        print("Day has ended!")
-
     def on_step(self, timestamp, d):
         """Generates trading signals based on RSI column already available in the dataset."""
         actions = []
